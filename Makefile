51a9ae98adcb707b8a67729d2b37bdb1ba03c49e
root <root@DESKTOP-VQ97IS2.localdomain>
2020-11-26 20:10:27 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+#this is sample
+testCal:paixuxxl.h paixuxxl.c testpaixuxxl.c
+		gcc paixuxxl.h paixuxxl.c testpaixu.c -o testpaixu
---
+++ paixuxxl.h
@@ -0,0 +1,15 @@
+#ifndef paixuxxl.h
+#define paixuxxl.h
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+void Bubbleback(int a[],int n);
+void InsertSort(int a[],int n);
+void SelectSort(int a[],int n);
+int Partition(int a[],int low, int high);
+void QSort(int a[],int low,int high);
+void QuickSort(int a[],int n);
+
+#endif
