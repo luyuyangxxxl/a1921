9182aea33416ad7ee1a7bd02a2857cd15313c922
root <root@DESKTOP-VQ97IS2.localdomain>
2020-12-03 21:31:28 +0800

    first commit

--- Makefile
+++
@@ -1,3 +0,0 @@
-#this is sample
-testCal:paixuxxl.h paixuxxl.c testpaixuxxl.c
-		gcc paixuxxl.h paixuxxl.c testpaixu.c -o testpaixu
---
+++ testlianzhan.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include"lianzhan.h"
+
+int main(){
+	LinkStack *s=InitStack();
+	Push(s,80);
+	Push(s,90);
+	Pop(s);
+	Push(s,70);
+	printf("实验的数值为:%d",GetTop(s));
+	printf("\n");
+	linkstack();
+}
